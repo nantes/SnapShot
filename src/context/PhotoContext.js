9846b70c1b651bec29a12dce87b7b6a14b6b82fd
Ivan Cetta <icetta@gmail.com>
2021-06-01 18:35:38 +0200

    add simple cache for axios with axios-cache-adapter library

--- src/context/PhotoContext.js
+++ src/context/PhotoContext.js
@@ -1,20 +1,31 @@
 import React, { createContext, useState } from "react";
 import axios from "axios";
+import { setupCache } from 'axios-cache-adapter'
 import { apiKey } from "../api/config";
 export const PhotoContext = createContext();
 
-const PhotoContextProvider = props => {
+const cache = setupCache({
+  maxAge: 15 * 60 * 1000,
+  exclude: { query: false },
+})
+
+const api = axios.create({
+  adapter: cache.adapter
+})
+
+  const PhotoContextProvider = props => {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
   const [searchEntry, setSearchEntry] = useState("");
   const [toggleSearch, setToggleSearch] = useState(false);
   
   const runSearch = query => {
-    axios
+    api
       .get(
         `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
       )
-      .then(response => {
+      .then(async response => {
+        await cache.store.length()
         setImages(response.data.photos.photo);
         setLoading(false);
       })
